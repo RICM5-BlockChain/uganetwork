b0cb5a283ec9eb06e9a5679eb7b04137c5355185
Antoine Boisadam <antoineboisadam@gmail.com>
2018-03-09 09:45:24 +0100

    MacOS compatibility

--- generate.sh
+++ generate.sh
@@ -47,9 +47,9 @@ PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
 sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml docker-compose-cas.yaml
 # If MacOSX, remove the temporary backup of the docker-compose file
-if [ "$ARCH" == "Darwin" ]; then
-  rm docker-compose-e2e.yaml docker-compose-cas.yaml
-fi
+#if [ "$ARCH" == "Darwin" ]; then
+#  rm docker-compose-e2e.yaml docker-compose-cas.yaml
+#fi
 
 # Génération des artéfacts---------------------------------------------
 
